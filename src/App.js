7334a1f98965d13149e7196387406905126432ac
Derek Lim <dereklim3006@gmail.com>
2026-02-05 11:37:31 -0500

    feat: add Board component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
-export default function Square() {
+function Square() {
   return <button className="square">X</button>;
+}
+
+export default function Board() {
+  return (
+    <div className="board">
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
 }
